f57770cb6d816d03356b230ff2c4dbc129c6eba0
Yevhenbk <balagutrak.jenia@gmail.com>
2021-10-30 12:47:00 +0000

    concurrent workers

--- src/main.go
+++ src/main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	//running 4 workers calculating fib number for each concurrently
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+	close(jobs)
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+/*func counter() {
 
 	c1 := make(chan string)
 	c2 := make(chan string)
@@ -32,4 +66,4 @@ func main() {
 			fmt.Println(msg2)
 		}
 	}
-}
+}*/
